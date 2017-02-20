fdaffe75d0d5c8ca7bbb2207d5b372c450f2ab2f
MRSHAHEERXD <shaheerm@uni.coventry.ac.uk>
2017-02-20 17:15:33 +0000

    Add files via upload
    Code needs to be updated to new table forms

---
+++ bookingForm160217.Designer.cs
@@ -0,0 +1,383 @@
+﻿namespace Sphere_Booking_and_Check_in_System.Booking
+{
+    partial class bookingForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.time = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.cusID = new System.Windows.Forms.TextBox();
+            this.slopeID = new System.Windows.Forms.TextBox();
+            this.staffID = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.End = new System.Windows.Forms.Button();
+            this.label10 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button4 = new System.Windows.Forms.Button();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.timeCheck = new System.Windows.Forms.TextBox();
+            this.label13 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(213, 9);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(213, 33);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(170, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Check Customer ID:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 31);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(195, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Check Customer Email:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(319, 7);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Check";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(319, 31);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Check";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(401, 15);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "..........";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(401, 36);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(37, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "..........";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(133, 91);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(12, 91);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(53, 20);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Date:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(12, 121);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(52, 20);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Time:";
+            // 
+            // time
+            // 
+            this.time.Location = new System.Drawing.Point(133, 123);
+            this.time.Name = "time";
+            this.time.Size = new System.Drawing.Size(200, 20);
+            this.time.TabIndex = 12;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(12, 157);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(110, 20);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Staff ID(op):";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(12, 191);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(115, 20);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "Customer ID:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(12, 224);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(84, 20);
+            this.label9.TabIndex = 15;
+            this.label9.Text = "Slope ID:";
+            // 
+            // cusID
+            // 
+            this.cusID.Location = new System.Drawing.Point(133, 191);
+            this.cusID.Name = "cusID";
+            this.cusID.Size = new System.Drawing.Size(200, 20);
+            this.cusID.TabIndex = 16;
+            // 
+            // slopeID
+            // 
+            this.slopeID.Location = new System.Drawing.Point(133, 226);
+            this.slopeID.Name = "slopeID";
+            this.slopeID.Size = new System.Drawing.Size(200, 20);
+            this.slopeID.TabIndex = 17;
+            // 
+            // staffID
+            // 
+            this.staffID.Location = new System.Drawing.Point(133, 159);
+            this.staffID.Name = "staffID";
+            this.staffID.Size = new System.Drawing.Size(200, 20);
+            this.staffID.TabIndex = 18;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(339, 91);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(121, 156);
+            this.button2.TabIndex = 19;
+            this.button2.Text = "Save";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // End
+            // 
+            this.End.Location = new System.Drawing.Point(196, 494);
+            this.End.Name = "End";
+            this.End.Size = new System.Drawing.Size(75, 23);
+            this.End.TabIndex = 20;
+            this.End.Text = "Finish";
+            this.End.UseVisualStyleBackColor = true;
+            this.End.Click += new System.EventHandler(this.End_Click);
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(173, 249);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(98, 20);
+            this.label10.TabIndex = 21;
+            this.label10.Text = "Availability ";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(16, 326);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(448, 133);
+            this.dataGridView1.TabIndex = 22;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(12, 465);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(448, 23);
+            this.button4.TabIndex = 23;
+            this.button4.Text = "Check Availability";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Location = new System.Drawing.Point(133, 274);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker2.TabIndex = 24;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.Location = new System.Drawing.Point(12, 274);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(53, 20);
+            this.label11.TabIndex = 25;
+            this.label11.Text = "Date:";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label12.Location = new System.Drawing.Point(12, 303);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(52, 20);
+            this.label12.TabIndex = 26;
+            this.label12.Text = "Time:";
+            // 
+            // timeCheck
+            // 
+            this.timeCheck.Location = new System.Drawing.Point(133, 300);
+            this.timeCheck.Name = "timeCheck";
+            this.timeCheck.Size = new System.Drawing.Size(200, 20);
+            this.timeCheck.TabIndex = 27;
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label13.Location = new System.Drawing.Point(367, 250);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(46, 20);
+            this.label13.TabIndex = 28;
+            this.label13.Text = "Cost";
+            // 
+            // bookingForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(472, 529);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.timeCheck);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.dateTimePicker2);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.End);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.staffID);
+            this.Controls.Add(this.slopeID);
+            this.Controls.Add(this.cusID);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.time);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "bookingForm";
+            this.Text = "Booking Form";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox time;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox cusID;
+        private System.Windows.Forms.TextBox slopeID;
+        private System.Windows.Forms.TextBox staffID;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button End;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.TextBox timeCheck;
+        private System.Windows.Forms.Label label13;
+    }
+}
---
+++ bookingForm160217.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace Sphere_Booking_and_Check_in_System.Booking
+{
+    public partial class bookingForm : Form
+    {
+        bool isreg = false;
+        SqlConnection con; //connection
+        string connectionString;
+
+
+        public bookingForm()
+        {
+            InitializeComponent();
+            connectionString = ConfigurationManager.ConnectionStrings["Sphere_Booking_and_Check_in_System.Properties.Settings.mainDatabaseConnectionString"].ConnectionString;
+            //GetBookings();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            con = new SqlConnection(connectionString);
+            con.Open();
+            SqlCommand checkid = new SqlCommand();
+            checkid.CommandText = "select * from [Customer]";
+            checkid.Connection = con;
+
+            SqlDataReader rd = checkid.ExecuteReader();
+            while (rd.Read())
+            {
+                if (rd[0].ToString() == textBox1.Text) //the zero here is calling to the id col
+                { //here we are saying that if rd var is == to the value user input is a match then isreg before true.
+                    isreg = true;
+                    //we then print it out to the label.
+                }
+
+            }
+            if (isreg == true)
+            {
+                label3.Text = "Is registerd";
+            }
+            else
+            {
+                label3.Text = "Could not find";
+            }
+            isreg = false;
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            con = new SqlConnection(connectionString);
+            con.Open();
+            SqlCommand checkid = new SqlCommand();
+            checkid.CommandText = "select * from [Customer]";
+            checkid.Connection = con;
+
+            SqlDataReader rd = checkid.ExecuteReader();
+            while (rd.Read())
+            {
+                if (rd[4].ToString() == textBox2.Text) //the 4 here is calling to the email col
+                { //here we are saying that if rd var is == to the value user input is a match then isreg before true.
+                    isreg = true;
+                    //we then print it out to the label.
+                }
+
+            }
+            if (isreg == true)
+            {
+                label4.Text = "Is registerd";
+            }
+            else
+            {
+                label4.Text = "Could not find";
+            }
+            isreg = false;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string Queary = "insert into Booking (bookingDate,bookingTime,staffId,customerID,slopeID) values('" + this.dateTimePicker1.Text + "','" + this.time.Text + "','" + this.slopeID.Text + "','" + this.cusID.Text + "','" + this.staffID.Text + "');";
+            con = new SqlConnection(connectionString);
+            SqlCommand cmddatabase = new SqlCommand(Queary, con);
+            SqlDataReader myReader;
+            try
+            {
+                con.Open();
+                myReader = cmddatabase.ExecuteReader();
+                    MessageBox.Show("saved");
+                while (myReader.Read())
+                {
+
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            
+        }
+
+        private void End_Click(object sender, EventArgs e)
+        {
+            Form1 mainmenu = new Form1();
+            this.Hide(); //this while hide the main form.
+            mainmenu.ShowDialog(); //this opens booking form "bf"
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            con = new SqlConnection(connectionString); //new connection
+            SqlCommand com = new SqlCommand("select id,bookingDate, bookingTime from Booking where bookingDate =" +this.dateTimePicker2.Text+" and bookingTime ="+this.timeCheck.Text+");", con);
+            try
+            {
+                SqlDataAdapter grid = new SqlDataAdapter();
+                grid.SelectCommand = com;// this is the sql code i need
+                DataTable griddataset = new DataTable(); //creating the datatable
+                grid.Fill(griddataset);
+                BindingSource bsource = new BindingSource();
+
+                bsource.DataSource = griddataset;
+                dataGridView1.DataSource = bsource; //so the grid will have all data from b source 
+                grid.Update(griddataset); 
+            } catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        /* private void GetBookings()
+         {
+             using (con = new SqlConnection(connectionString))
+             using (SqlDataAdapter adapter = new SqlDataAdapter("SELECT bookingDate FROM Booking", con))
+             {
+
+                 DataTable datentime = new DataTable();
+                 adapter.Fill(datentime);
+
+                 Ava.DisplayMember = "name";
+                 Ava.ValueMember = "id";
+                 Ava.DataSource = datentime;
+             }
+         }*/
+
+
+
+    }
+}
+
+
+
