52a76e9207ff2176c4812f5f557824175f5a1e03
MRSHAHEERXD <shaheerm@uni.coventry.ac.uk>
2017-03-02 15:48:33 +0000

    Add files via upload
    Updated database no major changes had to update a few things as we discussed in our meeting.

---
+++ Sphere Booking and Check-in System/Sphere Booking and Check-in System/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace Sphere_Booking_and_Check_in_System
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.BookingNav = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(253, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "NOT FOR RELEASE";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 225);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(252, 46);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Session Management DEBUG";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // BookingNav
+            // 
+            this.BookingNav.Location = new System.Drawing.Point(12, 51);
+            this.BookingNav.Name = "BookingNav";
+            this.BookingNav.Size = new System.Drawing.Size(253, 47);
+            this.BookingNav.TabIndex = 2;
+            this.BookingNav.Text = "Booking";
+            this.BookingNav.UseVisualStyleBackColor = true;
+            this.BookingNav.Click += new System.EventHandler(this.BookingNav_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(276, 283);
+            this.Controls.Add(this.BookingNav);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "DEBUG MENU ";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button BookingNav;
+    }
+}
+
